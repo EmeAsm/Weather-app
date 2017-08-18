d03cbe29409f5a5da88e9491c9f46126d3411dec
EmeAsm <emerick@live.be>
2017-08-18 15:30:01 +0200

    Initial Commit

---
+++ Rainy ShinyTests/Rainy_ShinyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rainy_ShinyTests.swift
+//  Rainy ShinyTests
+//
+//  Created by Emerick Asmus on 18/08/17.
+//  Copyright © 2017 Emerick Asmus. All rights reserved.
+//
+
+import XCTest
+@testable import Rainy_Shiny
+
+class Rainy_ShinyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
